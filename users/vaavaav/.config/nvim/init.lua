05b281f7608bbc62867b351d0d44ff6d718bb3eb
vaavaav <the.jprp@gmail.com>
2025-06-06 15:22:39 +0100

    Replace tmux-nvim synergy plugins with just one

--- users/vaavaav/.config/nvim/init.lua
+++ users/vaavaav/.config/nvim/init.lua
@@ -110,17 +110,15 @@ require("lazy").setup({
     end,
   },
   {
-    "christoomey/vim-tmux-navigator",
-    lazy = false,
+    "aserowy/tmux.nvim",
     config = function()
-      vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>")
-      vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>")
-      vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>")
-      vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>")
+      require('tmux').setup({
+        resize = {
+          resize_step_x = 5,
+          resize_step_y = 5,
+        }
+      })
     end,
-  },
-  {
-    "RyanMillerC/better-vim-tmux-resizer",
   },
   {
     "nvim-lualine/lualine.nvim",
